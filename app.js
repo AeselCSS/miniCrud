3a4c4da34d6370631e97bf0d44b3f0cec81528f8
AliHMohammad <alihmohammad@hotmail.com>
2023-04-26 19:27:42 +0200

    Created detailed view for movie posters onclick

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
 "use strict";
 
 window.addEventListener("load", start);
-const endpoint =
-  "https://byca-crud-default-rtdb.europe-west1.firebasedatabase.app/";
+const endpoint = "https://byca-crud-default-rtdb.europe-west1.firebasedatabase.app/";
 
 async function start() {
-  const moviesArray = await getMovies(endpoint);
-  showMovies(moviesArray);
+    const moviesArray = await getMovies(endpoint);
+    showMovies(moviesArray);
 }
 
 async function getMovies(url) {
-  const response = await fetch(`${url}movies.json`);
-  const data = await response.json();
-  const preparedData = prepareData(data);
-  return preparedData;
+    const response = await fetch(`${url}movies.json`);
+    const data = await response.json();
+    const preparedData = prepareData(data);
+    return preparedData;
 }
 
 function prepareData(dataObject) {
-  const movies = [];
+    const movies = [];
 
-  for (const key in dataObject) {
-    const movie = dataObject[key];
-    movie.id = key;
-    movies.push(movie);
-  }
-  return movies;
+    for (const key in dataObject) {
+        const movie = dataObject[key];
+        movie.id = key;
+        movies.push(movie);
+    }
+    return movies;
 }
 
 function showMovies(movies) {
-  for (const movie of movies) {
-    showMovie(movie);
-  }
+    for (const movie of movies) {
+        showMovie(movie);
+    }
 }
 
 function showMovie(movie) {
-  const moviesContainer = document.querySelector("#movie-grid");
-  const movieHTML = /*html*/ `
+    const moviesContainer = document.querySelector("#movie-grid");
+    const movieHTML = /*html*/ `
   
   <article class="grid-item" > 
   <img src="${movie.poster}" >
@@ -43,16 +42,84 @@ function showMovie(movie) {
 
   `;
 
-  moviesContainer.insertAdjacentHTML("beforeend", movieHTML);
-  document
-    .querySelector("#movie-grid article:last-child")
-    .addEventListener("click", movieClicked);
+    moviesContainer.insertAdjacentHTML("beforeend", movieHTML);
+    document.querySelector("#movie-grid article:last-child").addEventListener("click", movieClicked);
 
-  function movieClicked(event) {
-    showMovieDialog(movie);
-  }
+    function movieClicked(event) {
+        showMovieDialog(movie);
+    }
 }
 
 function showMovieDialog(movie) {
-  document.querySelector("#dialog-modal").showModal();
+    document.querySelector("#dialog-modal").innerHTML = "";
+
+    const genreString = getGenreTagsAsString(movie.genreTags)
+
+    const section = /*html*/ `
+	<section class="movie-details-section">
+        <article class="movie-details-functions">
+            <button id="movie-update-btn">Update details</button>
+            <button id="movie-remove-btn">Remove</button>
+        </article>
+
+        <article class="movie-details-header">
+            <p>${movie.title}</p>
+            <p>${movie.year}</p>
+            <p>${movie.runtime} Minutes</p>
+            <p>${movie.score}</p>
+        </article>
+
+        <article class="movie-details-main">
+            <div>
+                <img src="${movie.poster}">
+            </div>
+
+            <div>
+                <iframe src="${movie.trailer}"></iframe>
+            </div>
+        </article>
+
+        <article class="movie-details-other">
+            <div>
+                <p>${genreString}</p>
+                <p>${movie.director}</p>
+                <p>${movie.description}</p>
+            </div>
+            <div>
+                <p>Actors:</p>
+                <ul id="movie-actor-list">
+                    
+                </ul>
+            </div>
+        </article>
+    </section>
+    `;
+    
+    document.querySelector("#dialog-modal").insertAdjacentHTML("beforeend", section)
+    populateActorList(movie.actorStars)
+
+    document.querySelector("#dialog-modal").showModal();
+}
+
+function getGenreTagsAsString(genreTags) {
+    let genreString = "";
+
+    for (let i = 0; i < genreTags.length; i++){
+        if (i === genreTags.length - 1) {
+            genreString += genreTags[i];
+        } else {
+            genreString += `${genreTags[i]}, `;
+        }
+    }
+
+    return genreString
+}
+
+function populateActorList(actors) {
+    for (const actor of actors) {
+        const html = /*html*/`
+        <li>${actor}</li>
+        `
+        document.querySelector("#movie-actor-list").insertAdjacentHTML("beforeend", html);
+    }
 }
